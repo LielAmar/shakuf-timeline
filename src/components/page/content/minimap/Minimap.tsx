bc5b42f1f1716933e8b30e5c8cb4367048403da4
galba <gal.bahary@mail.huji.ac.il>
2024-02-28 20:02:53 +0200

    Added basic minimap

--- src/components/page/content/minimap/Minimap.tsx
+++ src/components/page/content/minimap/Minimap.tsx
@@ -1,4 +1,5 @@
 import { Content } from "@/types/content";
+import MinimapContent from "./MinimapContent";
 
 const Minimap = ({content}: {content: Content[] | null}) => {
   if (!content) {
@@ -7,7 +8,32 @@ const Minimap = ({content}: {content: Content[] | null}) => {
 
   return (
     <div className="w-full fixed">
-      this is text
+      {content.map((contentPeice: Content, index: number) => {
+        if (index == 0)
+          return (
+            <MinimapContent 
+              key={index}
+              content={contentPeice}
+              position={"first"}
+            />
+          );
+        else if (index == content.length - 1)
+          return (
+            <MinimapContent
+              key={index}
+              content={contentPeice}
+              position={"last"}
+            />
+          );
+        else
+          return (
+            <MinimapContent 
+              key={index}
+              content={contentPeice}
+              position={null}
+            />
+          );
+      })}
     </div>
   );
 };
